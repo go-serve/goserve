c3137dbcc61c03a974375be626e24b180ffd4449
Koala Yeung <koalay@gmail.com>
2016-11-30 15:27:51 +0800

    assets: Use bindatafs
    Got rid of local FileSystem implementation and use bindatafs.

--- assets/fs.go
+++ assets/fs.go
@@ -1,57 +1,13 @@
 package assets
 
 import (
-	"bytes"
 	"net/http"
-	"os"
-	"strings"
-)
 
-const (
-	TypeFile = iota
-	TypeDir
+	"github.com/go-serve/bindatafs"
+	"golang.org/x/tools/godoc/vfs/httpfs"
 )
 
 // FileSystem returns a http.FileSystem of the assets
 func FileSystem() (fs http.FileSystem) {
-	return &fileSystem{}
-}
-
-type fileSystem struct {
-}
-
-// Open the given string or return error
-func (fs *fileSystem) Open(name string) (f http.File, err error) {
-
-	if name[0] == '/' {
-		name = name[1:]
-	}
-
-	// test if is an unempty dir
-	names, _ := AssetDir(name)
-	if len(names) != 0 {
-		f = &File{
-			bytes.NewReader([]byte{}),
-			name,
-			TypeDir,
-			0,
-		}
-		return
-	}
-
-	// test if is a file
-	buf, err := Asset(name)
-	if err != nil {
-		if strings.HasSuffix(err.Error(), "not found") {
-			err = os.ErrNotExist
-		}
-		return
-	}
-	f = &File{
-		bytes.NewReader(buf),
-		name,
-		TypeFile,
-		0,
-	}
-	return
+	return httpfs.New(bindatafs.New("assets://", Asset, AssetDir, AssetInfo))
 }
